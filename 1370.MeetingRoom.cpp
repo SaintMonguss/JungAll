15c467c3c7bd8b5ae62e48eacf0012c612c7961e
용훈 황 <hyhhyhhyh@naver.com>
2022-08-24 16:53:26 +0900

    http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&code=1370&sca=3050

--- 1370.MeetingRoom.cpp
+++ 1370.MeetingRoom.cpp
@@ -1,39 +1,67 @@
 #include <iostream>
 #include <vector>
 
+class Meeting
+{
+public:
+    int number;
+    int startTime;
+    int endTime;
+
+    Meeting(int x, int y, int z) : number(x), startTime(y), endTime(z) {};
+    Meeting() {};
+};
+
 class MeetingRoom
 {
 public :
-	int** information;
+	Meeting* information;
     int meeting = 0;    //회의 갯수
     int finish_time;
-    std::vector<int> answer;
+    std::vector<Meeting> answer;
 
     void GetInfo()
     {
+        int number_tmp;
+        int start_tmp;
+        int end_tmp;
+
         for (int i = 0; i < meeting; i++)
         {
-            std::cin >> information[i][0];  //미팅 번호
-            std::cin >> information[i][1];  // 시작 시간
-            std::cin >> information[i][2];  //끝나는 시간
+           
+            std::cin >> number_tmp;
+            std::cin >> start_tmp;
+            std::cin >> end_tmp;
+            information[i] = Meeting(number_tmp, start_tmp, end_tmp);
         }
     }
 
+    void Answer()
+    {   
+        for (int i = 0; i < meeting; i++)
+        {
+            if (information[i].startTime >= finish_time)
+            {
+                answer.push_back(information[i]);
+                finish_time = information[i].endTime;
+            }
+        }
+    }
 };
 
 
 class SelectionSort
 {
-    static void selectionSort(int *arr)
+public:
+    static void selectionSort(Meeting* arr, int length)
     {
-        int length = sizeof(*arr) / 4;
         for (int i = 0; i < length - 1; i++)
         {
-            int temp = arr[i];
+            Meeting temp = arr[i];
             int num = i;
             for (int j = 1 + i; j < length; j++)
             {
-                if (temp > arr[j]) // 오름차순
+                if (temp.endTime > arr[j].endTime) // 오름차순
                 {
                     temp = arr[j];
                     num = j;
@@ -53,13 +81,15 @@ int main()
 {
     MeetingRoom meetingRoom;
     std::cin >> meetingRoom.meeting;
-    meetingRoom.information = new int*[meetingRoom.meeting];
-    for (int i = 0; i < meetingRoom.meeting ; i++)
-    {
-        meetingRoom.information[i] = new int[3];
-    }  // 2차원 배열 동적 할당
+    meetingRoom.information = new Meeting [meetingRoom.meeting];
     meetingRoom.GetInfo();
-
+    SelectionSort::selectionSort(meetingRoom.information, meetingRoom.meeting);
+    meetingRoom.Answer();
+    std::cout << meetingRoom.answer.size() << std::endl;
+    for (int i = 0; i < meetingRoom.answer.size(); i++)
+    {
+        std::cout << meetingRoom.answer[i].number << " ";
+    }
 }
 
 //meeting 클래스 생성 해서 배치하기
